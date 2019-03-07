8af87ca7ef7a5ae26eaccf66363a0de147174311
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-07 20:35:16 +0000

    Add Item

--- item_catalog/models.py
+++ item_catalog/models.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-""" """
+"""Python Database API."""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = 'item'
+    name = Column(
+        String(80), nullable=False
+    )
+    id = Column(
+        Integer, primary_key=True
+    )
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
