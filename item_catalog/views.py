fb1a59c78ba3b41c7df29a197ff6f890437efda8
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-08 17:54:56 +0000

    Add routes for the basic webdesign

--- item_catalog/views.py
+++ item_catalog/views.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
-""" """
+"""Backend of Item Catalog app."""
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Return index page."""
+    return render_template('index.html')
+
+
+@app.route('/login/')
+def login():
+    """Return page with login options."""
+    return render_template('index.html')
+
+
+@app.route('/categories/')
+def showCategories():
+    """Return categories page."""
+    return render_template('index.html')
+
+
+@app.route('/category/<int:categoryid>/')
+def showCategoryItems(categoryid):
+    """Return page with all items in a category."""
+    return render_template('index.html')
+
+
+@app.route('/category/<int:categoryid>/item/<int:itemid>/')
+def showItems(categoryid, itemid):
+    """Return page with description of specific item."""
     return render_template('index.html')
 
 
 def main():
-    """"""
+    """Serve up a webpage on localhost."""
     app.secret_key = 'key'
     app.run('localhost', port=8080, debug=True)
 
