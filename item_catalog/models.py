6623d2beecf24b964d779b8e42e46e27b1967909
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-09 16:16:44 +0000

    Add user class

--- item_catalog/models.py
+++ item_catalog/models.py
@@ -20,14 +20,17 @@ class Mixin(object):
 Base = declarative_base(cls=Mixin)
 
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+
 class Category(Base):
     __tablename__ = 'category'
-    name = Column(
-        String(80), nullable=False
-    )
-    id = Column(
-        Integer, primary_key=True
-    )
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     @property
     def serialize(self):
         # Return object data in easily serialisable format
@@ -40,19 +43,11 @@ class Category(Base):
 
 class Item(Base):
     __tablename__ = 'item'
-    name = Column(
-        String(80), nullable=False
-    )
-    description = Column(
-        String(800), nullable=False
-    )
-    category_id = Column(
-        Integer, ForeignKey('category.id')
-    )
+    name = Column(String(80), nullable=False)
+    description = Column(String(800), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    id = Column(
-        Integer, primary_key=True
-    )
+    id = Column(Integer, primary_key=True)
     @property
     def serialize(self):
         # Return object data in easily serialisable format
